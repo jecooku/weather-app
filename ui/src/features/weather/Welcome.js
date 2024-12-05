fb4129631b959579b1f8a2cd049a2565985bff32
Jorge Cook <jecook.u@gmail.com>
2024-12-05 12:22:35 -0500

    Fix minor css bug

--- ui/src/features/weather/Welcome.js
+++ ui/src/features/weather/Welcome.js
@@ -3,7 +3,7 @@ function Welcome() {
     <div className="flex flex-col">
       <div className="bg-sky-50" style={{ flex: 1 }}></div>
       <h1>Welcome to the weather App!</h1>
-      <p>
+      <p className="h-lvh">
         Please enter and select an address to see the weather in that location.
       </p>
     </div>
